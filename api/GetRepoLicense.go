8c669436707092bc9bcf272d41ebbd010f880974
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-02 22:42:13 +0530

    Create `GetRepoLicense` api function

---
+++ api/GetRepoLicense.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/cli/go-gh"
+)
+
+// Reference: https://docs.github.com/en/rest/licenses?apiVersion=2022-11-28#get-the-license-for-a-repository
+
+// TODO: Add documentation
+// TODO: Seems to return LicenseContent struct. See the API reference.
+func GetRepoLicense(owner, repo string) (License, error) {
+
+	// Instantiate the gh client
+	client, err := gh.RESTClient(nil)
+	if err != nil {
+		return License{}, err
+	}
+
+	// Set the license endpoint
+	endpoint := "repos/" + owner + "/" + repo + "/license"
+
+	// Fetch the license from the GitHub API
+	response := License{} // Create a License struct to hold the response
+	err = client.Get(endpoint, &response)
+	if err != nil {
+		return License{}, err
+	}
+
+	// Return the license
+	return response, nil
+
+}
