ce2fd1b20315dd8988ba281ef47fecae87be9be0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-07 16:07:06 +0530

    Create the `LicenseContent` type

--- api/GetRepoLicense.go
+++ api/GetRepoLicense.go
@@ -6,24 +6,23 @@ import (
 
 // Reference: https://docs.github.com/en/rest/licenses?apiVersion=2022-11-28#get-the-license-for-a-repository
 
-// TODO: Add documentation
-// TODO: Seems to return LicenseContent struct. See the API reference.
-func GetRepoLicense(owner, repo string) (License, error) {
+// Fetch a license of a repository from the GitHub API
+func GetRepoLicense(owner, repo string) (LicenseContent, error) {
 
 	// Instantiate the gh client
 	client, err := gh.RESTClient(nil)
 	if err != nil {
-		return License{}, err
+		return LicenseContent{}, err
 	}
 
 	// Set the license endpoint
 	endpoint := "repos/" + owner + "/" + repo + "/license"
 
 	// Fetch the license from the GitHub API
-	response := License{} // Create a License struct to hold the response
+	response := LicenseContent{} // Create a License struct to hold the response
 	err = client.Get(endpoint, &response)
 	if err != nil {
-		return License{}, err
+		return LicenseContent{}, err
 	}
 
 	// Return the license
--- api/types.go
+++ api/types.go
@@ -1,5 +1,7 @@
 package api
 
+// TODO: Implement Stringer interface for these structs
+
 // Reference: https://docs.github.com/en/rest/licenses?apiVersion=2022-11-28
 
 // Details of a particular license from the GitHub API
@@ -26,4 +28,25 @@ type LicenseSimple struct {
 	Spdx_id string `json:"spdx_id"` // The license SPDX ID (e.g. "MIT")
 	Url     string `json:"url"`     // The URL to the license on the web
 	Node_id string `json:"node_id"` // The node ID for the license
+}
+
+// Details of a particular license from the GitHub API
+type LicenseContent struct {
+	Name         string `json:"name"`         // The license name (e.g. "MIT License")
+	Path         string `json:"path"`         // The path to the license file
+	Sha          string `json:"sha"`          // The SHA of the license file
+	Size         int    `json:"size"`         // The size of the license file
+	Url          string `json:"url"`          // The URL to the license file
+	Html_url     string `json:"html_url"`     // The URL to the license file on the web
+	Git_url      string `json:"git_url"`      // The URL to the license file in the Git repository
+	Download_url string `json:"download_url"` // The URL to download the license file
+	Type_        string `json:"type"`         // The type of the license file
+	Content      string `json:"content"`      // The content of the license file
+	Encoding     string `json:"encoding"`     // The encoding of the license file
+	Links        struct {
+		Self string `json:"self"` // The URL to the license file
+		Git  string `json:"git"`  // The URL to the license file in the Git repository
+		Html string `json:"html"` // The URL to the license file on the web
+	} `json:"_links"` // The links to the license file
+	License LicenseSimple `json:"license"` // The license details
 }
