5df13ebff5c27e48f004c1cd0e5bc9f50357c832
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-08 23:01:49 +0530

    Improve Markdown Generator

--- docs/GenerateMarkdown.go
+++ docs/GenerateMarkdown.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -15,23 +16,21 @@ func GenerateMarkdown(cmd *cobra.Command, w io.Writer) error {
 	buf := new(bytes.Buffer)
 
 	// Write the title
-	buf.WriteString("# `" + cmd.CommandPath() + "`\n\n")
+	buf.WriteString("# `" + cmd.Name() + "`\n\n")
 
 	// Write the short description
 	buf.WriteString(cmd.Short + "\n\n")
+
+	// Write the aliases
+	if len(cmd.Aliases) > 0 {
+		buf.WriteString("## Aliases\n\n")
+		buf.WriteString("`" + strings.Join(cmd.Aliases, "`, `") + "`\n\n")
+	}
 
 	// Write the long description
 	if cmd.Long != "" {
 		buf.WriteString("## Description\n\n")
 		buf.WriteString(cmd.Long + "\n\n")
-	}
-
-	// Write the aliases
-	if len(cmd.Aliases) > 0 {
-		buf.WriteString("## Aliases\n\n")
-		buf.WriteString("```sh\n")
-		buf.WriteString(cmd.Name() + " " + cmd.Aliases[0] + "\n")
-		buf.WriteString("```\n\n")
 	}
 
 	// Write the usage
@@ -45,19 +44,44 @@ func GenerateMarkdown(cmd *cobra.Command, w io.Writer) error {
 	// Write the flags
 	if cmd.Flags().HasFlags() {
 		buf.WriteString("## Flags\n\n")
-		buf.WriteString("```sh\n")
-		cmd.Flags().VisitAll(func(f *pflag.Flag) {
-			buf.WriteString("--" + f.Name + " " + f.DefValue + " " + f.Usage + "\n")
+
+		// Create a markdown table
+		buf.WriteString("| Flag | Description | Default |\n")
+		buf.WriteString("|------|-------------|---------|\n")
+
+		// Write the flags to the table
+		cmd.Flags().VisitAll(func(flag *pflag.Flag) {
+			buf.WriteString("| `--" + flag.Name + ", -" + flag.Shorthand + "` | " + flag.Usage + " | " + flag.DefValue + " |\n")
 		})
-		buf.WriteString("```\n\n")
+		buf.WriteString("\n")
 	}
 
 	// Write the examples
 	if len(cmd.Example) > 0 {
 		buf.WriteString("## Examples\n\n")
 		buf.WriteString("```sh\n")
-		buf.WriteString(cmd.Example + "\n")
+		buf.WriteString(cmd.Example)
 		buf.WriteString("```\n\n")
+	}
+
+	// Write the see also
+	buf.WriteString("## See Also\n\n")
+
+	// Link to the parent command
+	if cmd.HasParent() {
+		buf.WriteString("* [`" + cmd.Parent().Name() + "`](./" + cmd.Parent().Name() + ".md)\n")
+
+		// Link to the other sibling commands
+		for _, subCmd := range cmd.Parent().Commands() {
+			if subCmd != cmd {
+				buf.WriteString("* [`" + subCmd.Name() + "`](./" + subCmd.Name() + ".md)\n")
+			}
+		}
+	}
+
+	// Link to the child commands
+	for _, subCmd := range cmd.Commands() {
+		buf.WriteString("* [`" + subCmd.Name() + "`](./" + subCmd.Name() + ".md)\n")
 	}
 
 	// Write the buffer to the writer
